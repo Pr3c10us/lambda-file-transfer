342f431fc47eec6c60a93f6198aa74554687d2a5
Owolabi precious <owo.pre.eno@gmail.com>
2023-07-10 10:58:44 +0100

    fixed lambda issue

--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,7 @@
 resource "aws_lambda_function" "lambda" {
   function_name    = var.name
   filename         = data.archive_file.lambda.output_path
-  source_code_hash = data.archive_file.lambda..output_base64sha256
+  source_code_hash = data.archive_file.lambda.output_base64sha256
   role             = aws_iam_role.role.arn
   handler          = "main.handler"
   runtime          = "python3.9"
